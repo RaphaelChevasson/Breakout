6956bee6482e4ebdd46d321d310fd5e8452345c9
RaphaelChevasson <rc@clubcapelli.com>
2018-05-18 18:27:52 +0200

    Corrected wrong paddle coordonates

--- sources/paddle.cpp
+++ sources/paddle.cpp
@@ -30,43 +30,46 @@ void Paddle::display()
 
     // Diplay the paddle
 
+    float w = mWidth/2;
+    float h = mHeigth/2;
+
     glBegin(GL_QUADS);
 
             GLfloat yellow[4] = {1.0f, 1.0f, 1.0f, 1.0f};
-            GLfloat white[4] = {1.0f, 1.0f, 0.0f, 1.0f};
+            GLfloat white[4]  = {1.0f, 1.0f, 0.0f, 1.0f};
             glMaterialfv(GL_FRONT, GL_AMBIENT, white);
             glMaterialfv(GL_FRONT, GL_DIFFUSE, yellow);
 
 
-            glVertex3f(mX,mY, 0.0f);
-            glVertex3f(mX+mWidth, mY, 0.0f);
-            glVertex3f(mX+mWidth,mY+mHeigth, 0.0f);
-            glVertex3f(mX, mY+mHeigth, 0.0f);
+            glVertex3f(-w, -h, -w);
+            glVertex3f( w, -h, -w);
+            glVertex3f( w,  h, -w);
+            glVertex3f(-w,  h, -w);
 
-            glVertex3f(mX,mY, 3.0f);
-            glVertex3f(mX+mWidth, mY, 3.0f);
-            glVertex3f(mX+mWidth,mY+mHeigth, 3.0f);
-            glVertex3f(mX, mY+mHeigth, 3.0f);
+            glVertex3f(-w, -h,  w);
+            glVertex3f( w, -h,  w);
+            glVertex3f( w,  h,  w);
+            glVertex3f(-w,  h,  w);
 
-            glVertex3f(mX,mY, 0.0f);
-            glVertex3f(mX+mWidth, mY, 0.0f);
-            glVertex3f(mX+mWidth, mY, 3.0f);
-            glVertex3f(mX,mY, 3.0f);
+            glVertex3f(-w, -h, -w);
+            glVertex3f( w, -h, -w);
+            glVertex3f( w, -h,  w);
+            glVertex3f(-w, -h,  w);
 
-            glVertex3f(mX,mY+mHeigth, 0.0f);
-            glVertex3f(mX+mWidth, mY+mHeigth, 0.0f);
-            glVertex3f(mX+mWidth, mY+mHeigth, 3.0f);
-            glVertex3f(mX,mY+mHeigth, 3.0f);
+            glVertex3f(-w,  h, -w);
+            glVertex3f( w,  h, -w);
+            glVertex3f( w,  h,  w);
+            glVertex3f(-w,  h,  w);
 
-            glVertex3f(mX,mY, 0.0f);
-            glVertex3f(mX, mY+mHeigth, 0.0f);
-            glVertex3f(mX, mY+mHeigth, 3.0f);
-            glVertex3f(mX,mY, 3.0f);
+            glVertex3f(-w, -h, -w);
+            glVertex3f(-w,  h, -w);
+            glVertex3f(-w,  h,  w);
+            glVertex3f(-w, -h,  w);
 
-            glVertex3f(mX+mWidth,mY, 0.0f);
-            glVertex3f(mX+mWidth,mY+mHeigth, 0.0f);
-            glVertex3f(mX+mWidth, mY+mHeigth, 3.0f);
-            glVertex3f(mX+mWidth,mY, 3.0f);
+            glVertex3f( w, -h, -w);
+            glVertex3f( w,  h, -w);
+            glVertex3f( w,  h,  w);
+            glVertex3f( w, -h,  w);
 
     glEnd();
 
